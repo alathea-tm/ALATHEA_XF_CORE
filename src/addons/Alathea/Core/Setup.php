57372151c06f0f4af56203099801fedad0d840a2
Spark108 <kirito.asumo@gmail.com>
2022-09-23 17:14:27 +0300

    init core

---
+++ src/addons/Alathea/Core/Setup.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Alathea\Core;
+
+use XF\AddOn\AbstractSetup;
+use XF\AddOn\StepResult;
+
+class Setup extends AbstractSetup
+{
+    /**
+     * @param array $stepParams
+     * @return StepResult|null
+     */
+    public function install(array $stepParams = [])
+    {
+        // TODO: Implement install() method.
+    }
+
+    /**
+     * @param array $stepParams
+     * @return StepResult|null
+     */
+    public function upgrade(array $stepParams = [])
+    {
+        // TODO: Implement upgrade() method.
+    }
+
+    /**
+     * @param array $stepParams
+     * @return StepResult|null
+     */
+    public function uninstall(array $stepParams = [])
+    {
+        // TODO: Implement uninstall() method.
+    }
+}
